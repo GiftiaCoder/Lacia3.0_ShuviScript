7847c757cf6076aef91245a8101033364d7fb9b6
GiftiaCoder <18013399616@163.com>
2019-10-06 19:30:33 +0800

    new configure sample

--- sample.py
+++ sample.py
@@ -5,13 +5,14 @@ from base.logger import logger
 import tensorflow as tf
 
 script_text = 'input = (output)()input_method()' \
-              + 'output = (output)(offset)output_method(input.output)'
+              + 'out1 = (output)(offset)output_method(input.output)' \
+              + 'out2 = (output)(offset)output_method(out1.output)'
 
 class InputMethod(Method):
     def __init__(self, inputs, inputnodes, confs, conf, logger):
         super().__init__(inputs, inputnodes, confs, conf, logger)
 
-        self.output = tf.sqrt(tf.Variable([2, 3, 5, 7, 11, 13, 17, 19], dtype=tf.float32))
+        self.output = tf.Variable([2, 3, 5, 7, 11, 13], dtype=tf.float32)
         self.register_output('output', self.output)
 
 
@@ -19,23 +20,31 @@ class OutputMethod(Method):
     def __init__(self, inputs, inputnodes, confs, conf, logger):
         super().__init__(inputs, inputnodes, confs, conf, logger)
 
+        self.val_offset = conf['offset']
+
         self.offset = tf.placeholder(dtype=tf.float32)
         self.register_placeholder('offset', self.offset)
 
-        self.output = inputs[0] * tf.sin(inputs[0]) + self.offset
+        self.output = inputs[0] + self.offset
         self.register_output('output', self.output)
 
     def feed_dict(self, feeddict):
-        feeddict[self.offset] = 3.141
+        feeddict[self.offset] = self.val_offset
+
+    def conf(self, confs, conf):
+        self.val_offset = conf['offset']
 
 
 graph = shuvi.graph.Graph(script_text, {
     'input_method': InputMethod,
     'output_method': OutputMethod,
-}, logger=logger)
+}, logger=logger, conf_paths=[
+    'sample.json.conf'
+])
 
 with tf.Session() as sess:
     graph.init(sess, tf.global_variables_initializer())
+    graph.conf()
     print(graph.run(sess, 'input.output'))
-    print(graph.run(sess, 'output.output'))
-
+    print(graph.run(sess, 'out1.output'))
+    print(graph.run(sess, 'out2.output'))
