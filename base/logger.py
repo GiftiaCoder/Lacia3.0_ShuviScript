374817950348f48006b96c4c3f25a28171271942
GiftiaCoder <18013399616@163.com>
2019-10-06 19:36:26 +0800

    correct log path

--- base/logger.py
+++ base/logger.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-handler = logging.FileHandler('lacia.log')
+handler = logging.FileHandler('shuvi.log')
 formatter = logging.Formatter(
     '[%(levelname)s] %(asctime)s [%(filename)s:%(lineno)d] %(message)s',
     '%Y-%m-%d %H:%M:%S')
